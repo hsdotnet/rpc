e411a3c76d3d1127d8f7b4d6525eb994678d2f3f
hushuang <hushuang@yiguo.com>
2017-04-14 15:19:57 +0800

    add

---
+++ Framework.Rpc.Core/Register/IConsumerRegister.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using Framework.Rpc.Core.Cluster.LoadBalance;
+
+namespace Framework.Rpc.Core.Register
+{
+    public interface IConsumerRegister : IRegister
+    {
+        List<ServerInfo> FindService(string appName);
+    }
+}
---
+++ Framework.Rpc.Core/Register/Zookeeper/ConsumerZookeeperRegistry.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Collections.Generic;
+
+using Framework.Rpc.Core.Cluster.LoadBalance;
+
+namespace Framework.Rpc.Core.Register.Zookeeper
+{
+    public class ConsumerZookeeperRegistry : IConsumerRegister
+    {
+        private readonly ZookeeperInvoker _zookeeperInvoker;
+
+        private readonly ZooKeeperNet.ZooKeeper _zookeeper;
+
+        public ConsumerZookeeperRegistry(string registerAddress)
+        {
+            _zookeeperInvoker = new ZookeeperInvoker();
+
+            _zookeeper = _zookeeperInvoker.Create(registerAddress, Constant.ZK_SESSION_TIMEOUT, null);
+        }
+
+        public void RegisterService()
+        {
+            _zookeeperInvoker.CreatePersistentNode(_zookeeper, Constant.ROOT_PATH);
+
+            string providerPath = string.Concat(Constant.ROOT_PATH, Constant.CONSUMER_PATH);
+            _zookeeperInvoker.CreatePersistentNode(_zookeeper, providerPath);
+
+            //string servicePath = string.Concat(providerPath, "/", _cacheContainer.Application.Name);
+            //_zookeeperInvoker.CreatePersistentNode(_zookeeper, servicePath);
+
+            //string address = string.Concat(_cacheContainer.Application.Host, ":", _cacheContainer.Application.Host);
+            //_zookeeperInvoker.CreateEphemeralNode(_zookeeper, string.Concat(servicePath, "/", address), address);
+        }
+
+        public List<ServerInfo> FindService(string appName)
+        {
+            List<ServerInfo> serverInfos = new List<ServerInfo>();
+
+            string servicePath = string.Concat(Constant.ROOT_PATH, Constant.PROVIDER_PATH, "/", appName);
+
+            IEnumerable<string> address = _zookeeper.GetChildren(servicePath, false);
+
+            foreach (string addres in address)
+            {
+                string addressPath = string.Concat(servicePath, "/", addres);
+
+                byte[] bytes = _zookeeper.GetData(addressPath, false, null);
+
+                string addresInfo = Encoding.UTF8.GetString(bytes);
+
+                serverInfos.Add(new ServerInfo()
+                {
+                    Host = addresInfo.Split(':')[0],
+                    Port = int.Parse(addresInfo.Split(':')[0]),
+                    Weight = 1
+                });
+            }
+
+            return serverInfos;
+        }
+    }
+}
